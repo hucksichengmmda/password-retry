a6ea07fc410d8a59b7e034155466ac609fd1ecb8
rensicheng <472979947@qq.com>
2018-12-28 00:43:02 +0800

    basic version done

---
+++ 密码重试程序.py
@@ -0,0 +1,13 @@
+password = 'a123456'
+i = 3
+i = int(3)
+while True:
+    KEY = input('请输入密码：')
+    if KEY == password:
+	    print('登陆成功！')
+	    break
+    else:
+	    i = i - 1
+	    print('密码错误！还有' ,i,'次机会')
+	    if i == 0:
+		    break
