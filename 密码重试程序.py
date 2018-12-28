8b839d7457acfcabcee28be4c6781870094d8f85
rensicheng <472979947@qq.com>
2018-12-28 11:17:58 +0800

    Update

--- 密码重试程序.py
+++ 密码重试程序.py
@@ -1,7 +1,7 @@
 password = 'a123456'
 i = 3
 i = int(3)
-while True:
+while i > 0:
     KEY = input('请输入密码：')
     if KEY == password:
 	    print('登陆成功！')
@@ -9,5 +9,4 @@ while True:
     else:
 	    i = i - 1
 	    print('密码错误！还有' ,i,'次机会')
-	    if i == 0:
-		    break
+	    
